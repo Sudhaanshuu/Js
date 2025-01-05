97cd288f6c26c5caf6cdd353b2b033cc9ccbfd8e
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:05:43 +0530

    Basic Question practice for the chapter 2

---
+++ Practice_Code/08_practiceset_ch2.js
@@ -0,0 +1,69 @@
+//! Q1
+
+/*
+let age = 14;
+
+function isAge(a) {
+    if (age > 10 && age < 20) {
+        console.log("y");
+        return true;
+    } else {
+        console.log("N");
+        return false;
+    }
+}
+
+isAge(age);
+*/
+
+//! Q2
+/*
+let age = 17;
+switch (age) {
+    case 10:
+        console.log("Hi");
+        break;
+    case 12:
+        console.log("Bye");
+        break;
+    default:
+        console.log("Not");
+}
+        */
+
+
+//! Q3
+/*
+let num = 18;
+
+function check(num) {
+
+    if (num % 2 == 0 && num % 3 == 0) {
+        console.log("Yes");
+    } else {
+        console.log("Not");
+    }
+}
+check(num);
+*/
+//! Q4
+/*
+let num = 18;
+
+function check(num) {
+
+    if (num % 2 == 0 || num % 3 == 0) {
+        console.log("Yes");
+    } else {
+        console.log("Not");
+    }
+}
+check(num); */
+
+//! Q5
+/*
+age = 25
+console.log("You ", age > 18 ? "can drive" : "Can't drive");
+*/
+
+//! Q6
