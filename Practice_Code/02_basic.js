d0192242e7d0f0426f2f850b0a6cf2f03306b034
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:01:53 +0530

    code for the identifier

---
+++ Practice_Code/02_basic.js
@@ -0,0 +1,13 @@
+console.log('Program for the variable in the js')
+
+// var su = 418;
+let su = 418;
+console.log(su)
+su = 'Suku';
+console.log(su)
+
+//Example of some variable
+
+// let 5suku;
+let $ = 6
+console.log($)
