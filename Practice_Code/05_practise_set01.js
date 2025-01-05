93cce9d97c189801ef4b85b58742478ac40803f6
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:03:46 +0530

    Some basic questions from the Chapter 01

---
+++ Practice_Code/05_practise_set01.js
@@ -0,0 +1,41 @@
+//! Q1
+
+let suku = "Sudhanshu Kumar Prasad";
+let su = 418;
+console.log(suku + su);
+
+//! Q2
+console.log(typeof(suku + su));
+
+//! Q3
+
+const st = {
+    name: "Sudhanshu",
+    reg: 2201030100,
+    head: false
+}
+
+// st = 418;
+console.log(st);
+
+//! Q4
+
+st['It'] = true;
+console.log(st);
+// we can change the name even the its const
+st['name'] = "Kumar";
+console.log(st);
+
+
+//! Q5 
+
+const suDict = {
+    Human: "insaan",
+    Man: "aadmi",
+    Work: "kaam",
+    Why: "Kyuu",
+    You: "aap"
+}
+
+console.log(suDict);
+console.log(suDict.Human);
