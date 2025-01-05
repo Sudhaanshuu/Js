ad29ce73c8e998d1beadc8f21c1ac9cdd4562936
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:06:08 +0530

    Basics of looping in js

---
+++ Practice_Code/09_loops.js
@@ -0,0 +1,25 @@
+//^Program to first n natural numbers 
+/*
+let n = 25;
+
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum)
+*/
+let st = {
+        sud: 99,
+        kal: 29,
+        pa: 49,
+        sam: 70
+    }
+    //^ for in loop
+for (let m in st) {
+    console.log("Marks of ", m + "is " + st[m]);
+}
+
+//^ for of loop
+for (let n of "Sudhanshu") {
+    console.log(n);
+}
