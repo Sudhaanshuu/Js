c38df662ce88739671c45fec8aa6a990dd162608
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:04:26 +0530

    basic exp and relational operators

---
+++ Practice_Code/06_exp_condisnals.js
@@ -0,0 +1,26 @@
+console.log("New "); //^ stating
+
+let a = 23;
+let b = 89;
+
+console.log("A + B = ", a + b);
+console.log("A - B = ", a - b);
+console.log("A / B = ", a / b);
+console.log("A * B = ", a * b);
+console.log("A ** B = ", a ** b);
+console.log("A % B = ", a % b);
+console.log("A ++", a++);
+console.log(a);
+console.log("++A", ++a);
+console.log(a);
+
+
+//^ Compairlet 
+let s = 45;
+// let k = "45";
+let k = 78;
+
+console.log("s==k", s == k);
+console.log("s!==k", s !== k);
+console.log("s===k", s === k);
+console.log("s!=k", s != k);
