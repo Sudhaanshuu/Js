5331154d79c532b7a7a2253cab85751f32111e02
Your Name <22010310@cgu-odisha.ac.in>
2025-01-05 22:03:02 +0530

    Data types in js

---
+++ Practice_Code/04_primitive_datatype.js
@@ -0,0 +1,29 @@
+//! NN SS BB U 
+// Number Null String Symbole Big Int Boolean Undefined
+
+let s1 = 418;
+let s2 = null;
+let s3 = "Hi sudhanhsu Kumar";
+let s4 = Symbol("I Love");
+let s5 = BigInt("78699");
+let s6 = true;
+let s7;
+console.log(s1, s2, s3, s4, s5, s6, s7);
+
+
+//types 
+console.log("\n")
+console.log(typeof(s4));
+
+
+//^ Object exampe
+
+const suku = {
+    "Su": 418,
+    "si": true,
+    "ku": false,
+    "pa": "hi"
+}
+
+console.log("\n \n")
+console.log(suku["si"])
