c0d164cdf223497775a5128747fd50247bd67fe9
Your Name <22010310@cgu-odisha.ac.in>
2025-01-06 21:50:49 +0530

    js file

---
+++ Practice_Code/WEB/script.js
@@ -0,0 +1,11 @@
+console.log(console)
+
+console.error("Got the fuking error");
+console.info("Hi");
+console.assert(44 > 66);
+console.assert(44 < 66);
+console.warn("Love You");
+console.table(console);
+console.time("Sudhanshu")
+console.timeEnd("Sudhanshu")
+console.timeLog()
